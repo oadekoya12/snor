efd09c825f7e5463e1887ea415668248f48ebec9
Olawale Adekoya <olawale.adekoya@techsur.solution>
2021-07-30 15:52:00 -0400

    dockerfile

--- local/script.sh
+++ local/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 su postgres -c '/usr/bin/pg_ctl start -l /var/log/psql/logfile -D /usr/local/psql/data'
-su sonar bash -c '/opt/sonarqube/bin/linux-x86-64/sonar.sh start'
+su sonar -c '/opt/sonarqube/bin/linux-x86-64/sonar.sh start'
 while true; do  sleep 1000 
 done
