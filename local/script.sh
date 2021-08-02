ebe165ab9257751374b8b30500da8e0b16094af3
Olawale Adekoya <olawale.adekoya@techsur.solution>
2021-08-02 12:15:02 -0400

    update Dockereile and script files

--- local/script.sh
+++ local/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sysctl -w vm.max_map_count=262144
 su postgres -c '/usr/bin/pg_ctl start -l /var/log/psql/logfile -D /usr/local/psql/data'
 su sonar -c '/opt/sonarqube/bin/linux-x86-64/sonar.sh start'
 while true; do  sleep 1000 
